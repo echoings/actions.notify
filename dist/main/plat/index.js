7af3c519bf48d21ed6d4349278ff2d589fc7248b
zeroooooooo <abc.xyz.2be+github@gmail.com>
2020-12-12 22:12:04 +0800

    fix: bundle to one file use ncc

--- dist/main/plat/index.js
+++ dist/main/plat/index.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const custom_1 = __importDefault(require("./custom"));
 const lark_1 = __importDefault(require("./lark"));
 const slask_1 = __importDefault(require("./slask"));
 const telegram_1 = __importDefault(require("./telegram"));
-const custom_1 = __importDefault(require("./custom"));
 const Plat = {
     Lark: lark_1.default,
     Slack: slask_1.default,
@@ -14,4 +14,4 @@ const Plat = {
     Custom: custom_1.default,
 };
 exports.default = Plat;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvcGxhdC9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLGtEQUF5QjtBQUN6QixvREFBMkI7QUFDM0IsMERBQWlDO0FBQ2pDLHNEQUE2QjtBQUU3QixNQUFNLElBQUksR0FFTjtJQUNGLElBQUksRUFBSixjQUFJO0lBQ0osS0FBSyxFQUFMLGVBQUs7SUFDTCxRQUFRLEVBQVIsa0JBQVE7SUFDUixNQUFNLEVBQU4sZ0JBQU07Q0FDUCxDQUFBO0FBRUQsa0JBQWUsSUFBSSxDQUFBIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvcGxhdC9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLHNEQUE4QjtBQUM5QixrREFBMEI7QUFDMUIsb0RBQTRCO0FBQzVCLDBEQUFrQztBQUVsQyxNQUFNLElBQUksR0FFTjtJQUNGLElBQUksRUFBSixjQUFJO0lBQ0osS0FBSyxFQUFMLGVBQUs7SUFDTCxRQUFRLEVBQVIsa0JBQVE7SUFDUixNQUFNLEVBQU4sZ0JBQU07Q0FDUCxDQUFDO0FBRUYsa0JBQWUsSUFBSSxDQUFDIn0=
--- dist/module/plat/index.d.ts
+++ dist/module/plat/index.d.ts
@@ -1,7 +1,7 @@
+import Custom from './custom';
 import Lark from './lark';
 import Slack from './slask';
 import Telegram from './telegram';
-import Custom from './custom';
 declare const Plat: {
     [index: string]: typeof Lark | typeof Slack | typeof Telegram | typeof Custom;
 };
