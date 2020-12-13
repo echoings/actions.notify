408efc6a676ffb70e9937ab6af89d6211ed1c721
zeroooooooo <abc.xyz.2be+github@gmail.com>
2020-12-13 10:53:10 +0800

    feat: test docker

--- dist/main/plat/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Custom from './custom';
-import Lark from './lark';
-import Slack from './slask';
-import Telegram from './telegram';
-declare const Plat: {
-    [index: string]: typeof Lark | typeof Slack | typeof Telegram | typeof Custom;
-};
-export default Plat;
